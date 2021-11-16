e3e28b875a6529ecb298c194ef2bef07ce50e8f5
Terrence <49866855+terrense@users.noreply.github.com>
2021-11-16 22:02:46 +0300

    Update stock.cpp

--- Stock_example_from_book(C++Primer)/Project2/stock.cpp
+++ Stock_example_from_book(C++Primer)/Project2/stock.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "stock.h"
-
+//默认构造函数的使用
 Stock::Stock() {
 	std::cout << "Default constructor called.\n";
 	company = "no name";
@@ -8,7 +8,7 @@ Stock::Stock() {
 	share_val = 0.0;
 	total_val = 0.0;
 }
-
+//自定义的构造函数
 Stock::Stock(const std::string& co, long n, double pr) {
 	std::cout << "Constructor using " << co << " called.\n";
 	company = co;
@@ -23,7 +23,7 @@ Stock::Stock(const std::string& co, long n, double pr) {
 	set_tot();
 
 }
-
+//Destructor析构函数
 Stock::~Stock() {
 	std::cout << "Bye Bye! " << company << "\n";
 }
